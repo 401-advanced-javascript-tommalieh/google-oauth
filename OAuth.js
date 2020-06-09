59159cdde08b36c446e8f2593da472f76a3e7b28
Tommalieh <muhammedtommalieh@gmail.com>
2020-06-09 10:11:23 +0300

    fix 2.oh

--- OAuth.js
+++ OAuth.js
@@ -9,6 +9,7 @@ module.exports = async (req, res, next) => {
   try {
     
     console.log('hello from the middleware');
+    console.log(req.access_token);
     const token = req.access_token;
     
     const remoteUser = await getRemoteUserInfo(token);
